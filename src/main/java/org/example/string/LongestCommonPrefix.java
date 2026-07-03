4d95d8fdfcc43afa7393a9a3cfd6f92f64fccbad
sergeyermakovich <sergu9848@gmail.com>
2026-07-04 01:34:38 +0300

    extra comments removed v2

--- src/main/java/org/example/string/LongestCommonPrefix.java
+++ src/main/java/org/example/string/LongestCommonPrefix.java
@@ -2,11 +2,17 @@ package org.example.string;
 
 import junit.framework.Assert;
 
+import java.util.Arrays;
+
 public class LongestCommonPrefix {
     public static void main(String[] args) {
         Assert.assertEquals("fl", longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
         Assert.assertEquals("", longestCommonPrefix(new String[]{"dog", "racecar", "car"}));
         Assert.assertEquals("c", longestCommonPrefix(new String[]{"cir", "car"}));
+
+        Assert.assertEquals("fl", longestCommonPrefix2(new String[]{"flower", "flow", "flight"}));
+        Assert.assertEquals("", longestCommonPrefix2(new String[]{"dog", "racecar", "car"}));
+        Assert.assertEquals("c", longestCommonPrefix2(new String[]{"cir", "car"}));
     }
 
     public static String longestCommonPrefix(String[] strs) {
@@ -32,5 +38,22 @@ public class LongestCommonPrefix {
             counter = 0;
         }
         return stringBuilder.toString();
+    }
+
+    public static String longestCommonPrefix2(String[] strs) {
+        Arrays.sort(strs);
+        String result = "";
+        int i = 0;
+        int length = strs.length;
+
+        while (i < strs[0].length()) {
+            if (strs[0].charAt(i) == strs[length - 1].charAt(i))
+                result += strs[0].charAt(i);
+            else
+                break;
+            i++;
+        }
+
+        return result;
     }
 }
